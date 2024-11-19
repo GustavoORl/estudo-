6ba12029cf14083a10f7782ab3b32b70cef54581
GustavoORl <124843237+GustavoORl@users.noreply.github.com>
2024-11-19 12:33:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.getElementById('meuBotao').addEventListener('click', function() { var ceu = document.getElementsByClassName('ceu'); ceu.style.backgroundColor = ceu.style.backgroundColor === 'lightskyblue' ? 'lightgreen' : 'lightskyblue'; });
